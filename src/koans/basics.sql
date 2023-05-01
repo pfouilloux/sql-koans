40a1e909e4c32325c47e74fdf1700c7ef1a2c5ea
Pierre Fouilloux <pfxdev@pm.me>
2023-05-01 12:31:20 +1000

    Done

--- src/koans/basics.sql
+++ src/koans/basics.sql
@@ -2,22 +2,22 @@
 SELECT 1
 
 -- Meditate on lower-case queries
-_____ 1
+select 1
 
 -- Meditate on selecting all columns from a table
 select * from book
 
 -- Meditate on selecting one column (title) from a table
-select _____ from book
+select title from book
 
 -- Meditate on selecting some columns (title, available) from a table
-select _____, _____ from book
+select title, available from book
 
 -- Meditate on adding a new book
 insert into book(title, genre, year_published, available)
-values('_____', '_____', _____, 1)
+values('The Hitchhiker''s Guide to the Galaxy', 'Science fiction', 1979, 1)
 
 -- Meditate on adding two new books
 insert into book(title, genre, year_published, available)
-values('_____', '_____', _____, 1)
-,('_____', '_____', _____, 1)
+values('The Restaurant at the End of the Universe', 'Science fiction', 1980, 1)
+,('Life, the Universe and Everything', 'Science fiction', 1982, 1)
